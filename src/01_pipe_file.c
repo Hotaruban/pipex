4f39ca6ff3372aaf26eec8d9b1d17863ec46af86
Jeremy Hurpy <jeremyhurpy@gmail.com>
2023-07-17 17:18:56 +0700

    change outfile if here_doc to append

--- src/01_pipe_file.c
+++ src/01_pipe_file.c
@@ -6,28 +6,39 @@
 /*   By: jhurpy <jhurpy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 00:33:46 by jhurpy            #+#    #+#             */
-/*   Updated: 2023/07/17 16:30:14 by jhurpy           ###   ########.fr       */
+/*   Updated: 2023/07/17 17:17:06 by jhurpy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/pipex.h"
 
-static void	child_process(t_data *data, char **env, int i)
+static void	open_file(t_data *data, int flag)
 {
-	int		fd;
+	int fd;
 
-	if (i == 0)
+	if (flag == 0)
 	{
 		fd = open(data->infile, O_RDONLY);
 		dup2(fd, STDIN_FILENO);
 		close(fd);
 	}
-	if (data->cmd[i + 2] == NULL)
+	else
 	{
-		fd = open(data->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		if (ft_strncmp(data->infile, "here_doc", 9) == 0)
+			fd = open(data->outfile, O_WRONLY | O_CREAT | O_APPEND, 0644);
+		else
+			fd = open(data->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 		dup2(fd, STDOUT_FILENO);
 		close(fd);
 	}
+}
+
+static void	child_process(t_data *data, char **env, int i)
+{
+	if (i == 0)
+		open_file(data, 0);
+	if (data->cmd[i + 2] == NULL)
+		open_file(data, 1);
 	if (i > 0)
 		dup2(data->tmpfd, STDIN_FILENO);
 	if (data->cmd[i + 2] != NULL)
