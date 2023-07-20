f6bd4f205613181567a0ace827f3796b76d9df52
Jeremy Hurpy <jeremyhurpy@gmail.com>
2023-07-20 23:06:29 +0700

    split mandatory and bonus

---
+++ mandatory/include/pipex.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhurpy <jhurpy@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/12 14:12:34 by jhurpy            #+#    #+#             */
+/*   Updated: 2023/07/20 21:40:06 by jhurpy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "../libft/includes/libft.h"
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+# include <sys/errno.h>
+# include <sys/types.h>
+
+int		main(int ac, char **av, char **ev);
+void	execute_cmd(char *av, char **ev);
+void	exit_error(char *str);
+
+#endif
